96a799ae81a931087075e6f62103a015343c0d32
Anikman333 <37609540+Anikman333@users.noreply.github.com>
2023-06-15 14:17:31 +0530

    Recent Commit

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
-#define URL route for index() view 
 from django.urls import path
 from . import views
 
+app_name = 'restaurant'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/',views.MenuItemView.as_view(), name='menu-list-create'),
+    path('menu<int:pk>/',views.SingleMenuItemView.as_view(), name='menu-retrieve-update-delete'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Menu
+from rest_framework import generics
+from .serializers import MenuSerializer
+from .models import Booking
+from rest_framework import viewsets
+from .serializers import BookingSerializer
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {} )
+
+# API views
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer   
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer    
